63efe3ff122761e65a4b5304d80f0f6a06fc687f
Denis Dvornikov <denisdvornikov@Mac-mini-Admin-3.local>
2018-02-09 17:17:55 +0300

    I am not sure should I add cmake-build-debug to .gitignore

---
+++ cmake-build-debug/CMakeFiles/sockets.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sockets.dir/main.c.o"
+  "CMakeFiles/sockets.dir/cmd/init.c.o"
+  "CMakeFiles/sockets.dir/pkg/errors.c.o"
+  "sockets.pdb"
+  "sockets"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sockets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
